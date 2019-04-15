fc13aebd304e5ba258295b71dae477ed22b9f8e5
Abd-Allah Eid <abdallah.mohamedfcih@gmail.com>
2019-04-15 21:49:16 +0200

    codeBase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RoomList from './components/RoomList'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import NewRoomForm from './components/NewRoomForm'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+          <RoomList />
+          <MessageList />
+          <SendMessageForm />
+          <NewRoomForm />
+        
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class RoomList extends React.Component {
+    render () {
+        return (
+            <div className="rooms-list">
+                <div className="help-text">RoomList</div>
+            </div>
+        )
+    }
+}
+export default RoomList
