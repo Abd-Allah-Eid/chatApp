07f019f6a66d8935f1906fefdff73dd619de8290
Abd-Allah Eid <abdallah.mohamedfcih@gmail.com>
2019-04-15 22:07:07 +0200

    rendering some hard coded messages from an array of objects- no aip used yet

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import Message from './Message';
+
+const DUMMY_DATA = [
+    {
+        senderId: 'perborgen',
+        text: 'Hey, how is it going?'
+    },
+    {
+        senderId: 'janedoe',
+        text: 'Great! How about you?'
+    },
+    {
+        senderId: 'perborgen',
+        text: 'Good to hear! I am great as well'
+    }
+]
+
+var Messages = DUMMY_DATA.map(item => {
+    return <p>{item.senderId}: {item.text}</p>
+})
 
 class MessageList extends React.Component {
     render() {
         return (
             <div className="message-list">
-                <div className="help-text">MessageList</div>
+                {Messages}
             </div>
         )
     }
