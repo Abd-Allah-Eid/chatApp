11a29d96cdb9e52f1d9c0ed02c600c3917c7a3d8
Abd-Allah Eid <abdallah.mohamedfcih@gmail.com>
2019-04-16 19:13:26 +0200

    setup sendMessageForm component for broadcasting messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-class Message extends React.Component {  
-    render() {
-        return (
-            <div className="message">
-            </div>
-        )
-    }
+function Message (props) {
+    console.log(props)
+    return (
+        <div className="message">
+            <div className="message-username">{props.username}</div>
+            <div className="message-text">{props.text}</div>
+        </div>
+    )
 }
 
 export default Message
--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,12 +1,40 @@
 import React from 'react'
 
 class SendMessageForm extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            textMessage: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+        
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.state.textMessage)
+    }
     render() {
+        // console.log(this.state.textMessage)
         return (
-            <form className="send-message-form">
+            <form 
+                className="send-message-form"
+                onSubmit={this.handleSubmit}
+            >
                 <input
-                    placeholder="SendMessageForm"
-                    type="text" />
+                    placeholder="type your message here and press ENTER"
+                    type="text"
+                    name="textMessage"
+                    value={this.state.textMessage}
+                    onChange={this.handleChange}
+                />
             </form>
         )
     }
